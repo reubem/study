8a01525dca582d4531d9f240c3d5da40f675bf37
Reubem Girardi <reubem@gmail.com>
2015-10-21 09:59:15 -0400

    Initial improvement.

--- WebProject/src/HelloWorld.java
+++ WebProject/src/HelloWorld.java
@@ -1,8 +1,11 @@
 
 public class HelloWorld {
 
+	/**
+	 * Implement application
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 	}
 
